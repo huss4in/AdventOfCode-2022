462961a8c682fb71567777a2eb3672a7839fdeff
Hussain <huss4in7@outlook.com>
2023-05-02 14:58:26 +0300

    fix(Day6): tests

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -4,11 +4,11 @@ use crate::Challenge;
 pub struct Day6;
 
 impl Challenge for Day6 {
-    fn part1(input: &str) -> String {
+    fn part1(_input: &str) -> String {
         "0".into()
     }
 
-    fn part2<'a>(input: &str) -> String {
+    fn part2<'a>(_input: &str) -> String {
         "0".into()
     }
 }
@@ -21,11 +21,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(Day6::part1(INPUT), "");
+        assert_eq!(Day6::part1(INPUT), "0");
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(Day6::part2(INPUT), "");
+        assert_eq!(Day6::part2(INPUT), "0");
     }
 }
