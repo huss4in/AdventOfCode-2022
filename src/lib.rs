7adb00da5be8e4555b79db03d3ef6c48e094e675
Hussain <huss4in7@outlook.com>
2023-05-10 13:16:50 +0300

    add(Day7)

--- src/lib.rs
+++ src/lib.rs
@@ -57,6 +57,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 pub use day1::Day1;
 pub use day2::Day2;
@@ -64,12 +65,14 @@ pub use day3::Day3;
 pub use day4::Day4;
 pub use day5::Day5;
 pub use day6::Day6;
+pub use day7::Day7;
 
-pub const DAYS: [fn(); 6] = [
+pub const DAYS: &[fn()] = &[
     Day1::run,
     Day2::run,
     Day3::run,
     Day4::run,
     Day5::run,
     Day6::run,
+    Day7::run,
 ];
