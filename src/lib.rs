1beb675e33efaa0b1dafdd8fb0fee3fbaa0f0f34
Hussain <huss4in7@outlook.com>
2023-05-10 13:13:29 +0300

    feat(Day6): Complete Day6

--- src/lib.rs
+++ src/lib.rs
@@ -58,9 +58,18 @@ mod day4;
 mod day5;
 mod day6;
 
-pub use day1::*;
-pub use day2::*;
-pub use day3::*;
-pub use day4::*;
-pub use day5::*;
-pub use day6::*;
+pub use day1::Day1;
+pub use day2::Day2;
+pub use day3::Day3;
+pub use day4::Day4;
+pub use day5::Day5;
+pub use day6::Day6;
+
+pub const DAYS: [fn(); 6] = [
+    Day1::run,
+    Day2::run,
+    Day3::run,
+    Day4::run,
+    Day5::run,
+    Day6::run,
+];
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 #![allow(non_snake_case)]
 
-use AdventOfCode::*;
-
 fn main() {
     println!();
 
-    Day1::run();
-    Day2::run();
-    Day3::run();
-    Day4::run();
-    Day5::run();
+    for day in AdventOfCode::DAYS {
+        day();
+    }
 }
